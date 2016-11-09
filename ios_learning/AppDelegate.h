89aebf2a82f5f096e1b5f81b51b2878d0f48c714
qiuwf <qiuwf@Ctrip.com>
2016-11-09 15:38:33 +0800

    Initial Commit

---
+++ ios_learning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios_learning
+//
+//  Created by Gideon on 11/9/16.
+//  Copyright © 2016 Gideon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios_learning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios_learning
+//
+//  Created by Gideon on 11/9/16.
+//  Copyright © 2016 Gideon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
